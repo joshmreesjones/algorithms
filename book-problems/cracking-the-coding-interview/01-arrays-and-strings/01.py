b6ba630e0bdc561b7487646e2bb8fc9dd53974eb
jmreesjo <jmreesjo@ncsu.edu>
2016-11-29 12:30:46 -0500

    Solve 1.1

--- book-problems/cracking-the-coding-interview/01-arrays-and-strings/01.py
+++ book-problems/cracking-the-coding-interview/01-arrays-and-strings/01.py
@@ -1,66 +1,69 @@
-# Implement an algorithm to determine if a string has all unique characters.
-# What if you cannot use additional data structures?
+"""
+Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures?
 
-def check_unique(string):
-    if len(string) > 128:
+1. Ask clarifying questions.
+    - How long can the string be?
+        - Assuming it can fit in memory with lots of room to spare.
+    - Is the string composed of ASCII characters?
+        - Assuming yes.
+
+2. Design an algorithm.
+    - Brute force: iterate through every pair of characters and see if any match. If any pair does, return False. Otherwise, return True.
+        - Time complexity: O(n^2)
+        - Space complexity: O(1)
+    - Sorting: sort the string by character. Iterate through each pair of characters and if any match, return False. Otherwise, return True.
+        - Time complexity: O(nlog(n))
+        - Space complexity: O(1), unless sorting takes extra space
+    - Small hash map: initialize an array of size 256 to all 0s. For every character, check if there is a 1 in that character's position in the array. If yes, return False. If no, change it to a 1.
+        - Time complexity: O(n)
+        - Space complexity: O(n)
+
+3. Write pseudocode.
+
+function unique(string)
+    if string length > 256
+        return false
+    else
+        array = [0] * 256
+        for character in string
+            if array[character] is 1
+                return false
+            else
+                array[character] = 1
+        return true
+
+4. Write real code.
+"""
+
+def unique(string):
+    if len(string) > 256:
         return False
+    else:
+        array = [0] * 256
+        for character in string:
+            index = ord(character)
+            if array[index] == 1:
+                return False
+            else:
+                array[index] = 1
+        return True
 
-    flags = [0 for i in range(128)]
+"""
+5. Test code.
+"""
 
-    for character in string:
-        index = ord(character)
-
-        if flags[index] > 0:
-            return False
-        else:
-            flags[index] += 1
-
-    return True
-
-
-test_strings = [
-    "abcdefghijklmnopqrstuvwxyz",
-    "abbc",
-    "abcb",
-    "aabc",
-    "abcc",
-    "",
-    "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-    "\n",
-    "a"
+tests = [
+    ("", True),
+    ("a", True),
+    ("aA", True),
+    ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", True),
+    ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZa", False),
+    ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZA", False),
+    ("  ", False)
 ]
-
-test_results = [
-    True,
-    False,
-    False,
-    False,
-    False,
-    True,
-    False,
-    True,
-    True
-]
-
-tests = zip(test_strings, test_results)
-successes = 0
-failures = 0
 
 for test in tests:
-    print("Testing string '%s'" % test[0])
+    assert unique(test[0]) == test[1]
 
-    expected = test[1]
-    result = check_unique(test[0])
-
-    print("\tExpected: %s" % expected)
-    print("\tResult: %s" % result)
-
-    if expected == result:
-        print("\tTest passed.")
-        successes += 1
-    else:
-        print("\tTest failed.")
-        failures += 1
-
-print("Successes: %i" % successes)
-print("Failures: %i" % failures)
+print("Tests passed.")
